1c0289a29a05293665c6069ba573598a4f3c4392
Abd-Al-Ra'ouf Al-Kilany <abd-al-raoufal-kilany@Abd-Al-Raoufs-MacBook-Pro.local>
2024-05-09 22:50:36 +0100

    f

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ export async function searchPeople(event) {
         const { data, error } = await supabase
             .from('people')
             .select('*')
-            .eq('name',nameOrLicense)
+            //.eq('name',nameOrLicense)
 
 
             // .select('*')
