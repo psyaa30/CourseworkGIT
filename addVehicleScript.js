fc68a44df6ac422c564a762e09e676f6a2cddc70
Abd-Al-Ra'ouf Al-Kilany <abd-al-raoufal-kilany@Abd-Al-Raoufs-MacBook-Pro.local>
2024-05-11 22:22:41 +0100

    l

--- addVehicleScript.js
+++ addVehicleScript.js
@@ -1,9 +1,8 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-const supabase = createClient('https://mfhrllsznlxvbhnxcvll.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1maHJsbHN6bmx4dmJobnhjdmxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUxMjYyOTQsImV4cCI6MjAzMDcwMjI5NH0.UbF_JOJIntL7oYhbkzr_k1P_1E_B0ulwtBEdEOquyS4');
+const supabase = createClient('https://mfhrllsznlxvbhnxcvll.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1maHJsbHN6bmx4dmJobnhjdmxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUxMjYyOTQsImV4cCI6MjAzMDcwMjI5NH0.UbF_JOJIntL7oYhbkzr_k1P_1E_B0ulwtBEdEOquyS4');
 
-document.getElementById('submitBtn').addEventListener('click', async () => 
-{
+document.getElementById('submitBtn').addEventListener('click', async () => {
     const addPlate = document.getElementById('addPlate').value.trim().toUpperCase();
     const addMake = document.getElementById('addMake').value.trim();
     const addModel = document.getElementById('addModel').value.trim();
@@ -13,35 +12,29 @@ document.getElementById('submitBtn').addEventListener('click', async () =>
     const resultDiv = document.querySelector('main > div.result');
 
     // Check if any field is empty
-    if (!addPlate || !addMake || !addModel || !addColour || !addID) 
-    {
+    if (!addPlate || !addMake || !addModel || !addColour || !addID) {
         resultDiv.innerText = 'Please fill in all of the boxs.';
         return;
     }
 
     // Construct the data object to be inserted into the database
-    const vehicleData = 
-    {
+    const vehicleData = {
         VehicleID: addPlate,
         Make: addMake,
         Model: addModel,
         Colour: addColour,
         OwnerID: addID
     };
-/////
-    try 
-    {
+    
+    try {
         // Insert the data into the database
         const { data, error } = await supabase.from('vehicles').insert([vehicleData]);
-        
 
-        if (error) 
-        {
+        if (error) {
             resultDiv.innerText = 'Error';
             console.error(error);
-        } 
-        else 
-        {
+        }
+        else {
             resultDiv.innerText = 'Vehicle added successfully!';
             // Clear the input fields after successful addition
             document.getElementById('addPlate').value = '';
@@ -50,9 +43,8 @@ document.getElementById('submitBtn').addEventListener('click', async () =>
             document.getElementById('addColour').value = '';
             document.getElementById('addID').value = '';
         }
-    } 
-    catch (error) 
-    {
+    }
+    catch (error) {
         console.error('Error adding vehicle:', error.message);
     }
 });
