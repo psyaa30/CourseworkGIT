2adaada932bb792c829386e1fb7c1314003002f0
Abd-Al-Ra'ouf Al-Kilany <abd-al-raoufal-kilany@Abd-Al-Raoufs-MacBook-Pro.local>
2024-05-13 00:51:38 +0100

    update

--- addVehicleScript.js
+++ addVehicleScript.js
@@ -58,8 +58,8 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 const supabase = createClient('https://mfhrllsznlxvbhnxcvll.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1maHJsbHN6bmx4dmJobnhjdmxsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUxMjYyOTQsImV4cCI6MjAzMDcwMjI5NH0.UbF_JOJIntL7oYhbkzr_k1P_1E_B0ulwtBEdEOquyS4');
 
 document.getElementById('submitVehicleBtn').addEventListener('click', async () => {
-    const ownerID = document.getElementById('owner').value.trim();
-    const ownerExists = await checkOwnerExists(ownerID);
+    const ownerName = document.getElementById('owner').value.trim();
+    const ownerExists = await checkOwnerExists(ownerName);
 
     if (!ownerExists) {
         // If owner does not exist, show the owner form
@@ -75,8 +75,8 @@ document.getElementById('submitOwnerBtn').addEventListener('click', async () => 
     await submitOwner();
 });
 
-async function checkOwnerExists(ownerID) {
-    const { data, error } = await supabase.from('people').select('*').eq('PersonID', ownerID);
+async function checkOwnerExists(ownerName) {
+    const { data, error } = await supabase.from('people').select('*').eq('Name', ownerName);
     return data.length > 0; // Returns true if owner exists, otherwise false
 }
 
